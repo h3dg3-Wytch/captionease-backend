867eec28d32905994af11353f97daf8e8fe1bf70
rex <rexpeters3n@gmail.com>
2021-09-15 13:07:48 -0400

    feat: add in writing to audio bucket

--- src/services/transcribe/extract-audio/config.ts
+++ src/services/transcribe/extract-audio/config.ts
@@ -2,38 +2,12 @@ export interface ConfigInterface {
   STAGE: string;
   NODE_ENV: string;
   SUPABASE_API_URL: string;
-  SUPABASE_API_KEY: string;
   VIDEO_INPUT_BUCKET: string;
   EXTRACTED_VIDEO_AUDIO_BUCKET: string;
 }
 
 export default {
   STAGE: {
-    doc: 'Development or production',
-    format: String,
-    default: null,
-  },
-  NODE_ENV: {
-    doc: 'Development or production',
-    format: String,
-    default: null,
-  },
-  SUPABASE_API_URL: {
-    doc: 'Development or production',
-    format: String,
-    default: null,
-  },
-  SUPABASE_API_KEY: {
-    doc: 'Development or production',
-    format: String,
-    default: null,
-  },
-  VIDEO_INPUT_BUCKET: {
-    doc: 'Development or production',
-    format: String,
-    default: null,
-  },
-  EXTRACTED_VIDEO_AUDIO_BUCKET: {
     doc: 'Development or production',
     format: String,
     default: null,
